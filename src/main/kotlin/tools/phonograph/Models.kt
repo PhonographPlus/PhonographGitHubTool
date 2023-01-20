e5589169fcff96371471ff4ce9312d6f14357a20
chr56 <_>
2023-01-20 20:12:22 +0800

    json: add json model `release`

---
+++ src/main/kotlin/tools/phonograph/Models.kt
@@ -0,0 +1,50 @@
+package tools.phonograph
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Release(
+    @SerialName("tag_name")
+    val tagName: String,
+    @SerialName("name")
+    val name: String,
+    @SerialName("prerelease")
+    val preRelease: Boolean,
+    @SerialName("draft")
+    val draft: Boolean,
+    @SerialName("body")
+    val body: String,
+    @SerialName("url")
+    val url: String,
+    @SerialName("html_url")
+    val htmlUrl: String?,
+    @SerialName("created_at")
+    val createdAt: String,
+    @SerialName("published_at")
+    val publishedAt: String,
+    @SerialName("assets")
+    val assets: List<Asset>?,
+) {
+    @Serializable
+    data class Asset(
+        @SerialName("name")
+        val name: String,
+        @SerialName("content_type")
+        val contentType: String,
+        @SerialName("size")
+        val size: Long,
+        @SerialName("browser_download_url")
+        val browserDownloadUrl: String,
+        @SerialName("download_count")
+        val downloadCount: Long,
+        @SerialName("label")
+        val label: String?,
+        @SerialName("created_at")
+        val createdAt: String,
+        @SerialName("updated_at")
+        val updatedAt: String?,
+        @SerialName("url")
+        val url: String,
+    )
+}
