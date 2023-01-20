a4e2e00c526d6638447a463464f2ec4ba5872096
chr56 <_>
2023-01-20 19:32:15 +0800

    implement json download

---
+++ src/main/kotlin/tools/phonograph/Download.kt
@@ -0,0 +1,29 @@
+package tools.phonograph
+
+import java.io.File
+
+private val rt = Runtime.getRuntime()
+
+
+fun download() {
+    println("Start downloading...")
+
+    val outputDir = File(".", "jsons")
+
+    for (tag in tags) {
+        println("Download $tag...")
+        val process =
+            rt.exec("""gh api -H "Accept: application/vnd.github+json" /repos/$OWNER/$REPO_NAME/releases/tags/$tag""")
+        val raw = process.inputStream.readBytes()
+
+        val file = File(outputDir, "$tag.release.json")
+        val json = String(raw, Charsets.UTF_8)
+
+        file.outputStream().buffered().writer(Charsets.UTF_8).use {
+            it.write(json)
+            it.flush()
+        }
+    }
+
+    println("Download completely")
+}
