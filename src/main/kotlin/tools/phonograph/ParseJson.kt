bc5c0f357521ba5873a9fdc8b9b847da1aaffa5f
chr56 <_>
2023-01-20 20:21:44 +0800

    implement json parse

---
+++ src/main/kotlin/tools/phonograph/ParseJson.kt
@@ -0,0 +1,20 @@
+package tools.phonograph
+
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.io.File
+
+private val parser = Json { ignoreUnknownKeys = true }
+
+fun parse(): List<Release> {
+    println("Start parsing jsons...")
+    val jsons = jsonOutputFiles.listFiles() ?: return emptyList()
+    val releases = jsons.map(::decode)
+    println("Parsing json completed!")
+    return releases
+}
+
+private fun decode(json: File): Release {
+    val text = json.readText()
+    return parser.decodeFromString(text)
+}
